32f794abe00a656fc8cf6574b74bc84ce994b67b
Cyprian Kings <kiamacyprian6@gmail.com>
2025-07-20 21:16:02 +0300

    Weather recommendation program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,4 @@
-current_weather = input(
-    "What's the weather like today? (sunny/rainy/cold): ").lower()
+current_weather = input("What's the weather like today? (sunny/rainy/cold):")
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 
